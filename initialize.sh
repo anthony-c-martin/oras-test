94ccadd032707121e9f5d9552a01bdcd9f940fd9
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2022-09-20 00:33:19 +0000

    Setup ACR & scripts to publish dummy types

---
+++ initialize.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+subId=<<FILL IN>>
+tenantId=<<FILL IN>>
+registryName=antregistry2
+rgName=ant-test
+repoSubject=repo:anthony-c-martin/oras-test:ref:refs/heads/main
+
+az deployment group create --resource-group $rgName --template-file ./bicep/acr.bicep --parameters registryName=$registryName
+
+appCreate=$(az ad app create --display-name $registryName)
+appId=$(echo $appCreate | jq -r '.appId')
+appOid=$(echo $appCreate | jq -r '.id')
+
+spCreate=$(az ad sp create --id $appId)
+spId=$(echo $spCreate | jq -r '.id')
+az role assignment create --role contributor --subscription $subId --assignee-object-id $spId --assignee-principal-type ServicePrincipal --scope /subscriptions/$subId/resourceGroups/ant-test
+
+az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$appOid/federatedIdentityCredentials" --body '{"name":"gh-oidc","issuer":"https://token.actions.githubusercontent.com","subject":"'$repoSubject'","description":"GitHub OIDC Connection","audiences":["api://AzureADTokenExchange"]}'
+
+echo "ACR_CLIENT_ID=$appId"
+echo "ACR_SUBSCRIPTION_ID=$subId"
+echo "ACR_TENANT_ID=$tenantId"
