9fd36f47655d92b3f2cdb2672eb5d9cee3354610
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2022-09-20 11:43:55 -0400

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -4,7 +4,8 @@ subId=<<FILL IN>>
 tenantId=<<FILL IN>>
 registryName=antregistry2
 rgName=ant-test
-repoSubject=repo:anthony-c-martin/oras-test:ref:refs/heads/main
+repoName=oras-test
+repoSubject=repo:anthony-c-martin/$repoName:ref:refs/heads/main
 
 az deployment group create --resource-group $rgName --template-file ./bicep/acr.bicep --parameters registryName=$registryName
 
@@ -16,8 +17,9 @@ spCreate=$(az ad sp create --id $appId)
 spId=$(echo $spCreate | jq -r '.id')
 az role assignment create --role contributor --subscription $subId --assignee-object-id $spId --assignee-principal-type ServicePrincipal --scope /subscriptions/$subId/resourceGroups/ant-test
 
-az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$appOid/federatedIdentityCredentials" --body '{"name":"gh-oidc","issuer":"https://token.actions.githubusercontent.com","subject":"'$repoSubject'","description":"GitHub OIDC Connection","audiences":["api://AzureADTokenExchange"]}'
+az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$appOid/federatedIdentityCredentials" --body '{"name":"'$repoName'","issuer":"https://token.actions.githubusercontent.com","subject":"'$repoSubject'","description":"GitHub OIDC Connection","audiences":["api://AzureADTokenExchange"]}'
 
-echo "ACR_CLIENT_ID=$appId"
-echo "ACR_SUBSCRIPTION_ID=$subId"
-echo "ACR_TENANT_ID=$tenantId"
+echo "Now configure the following GitHub Actions secrets:"
+echo "  ACR_CLIENT_ID: $appId"
+echo "  ACR_SUBSCRIPTION_ID: $subId"
+echo "  ACR_TENANT_ID: $tenantId"
